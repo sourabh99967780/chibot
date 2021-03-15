6d6daa771f8bf6098ca340c8c624de58b0a0b51b
Sourabh Kumar <sourabh99967780@gmail.com>
2021-03-16 00:28:52 +0530

    Some Command Changes

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -1,16 +1,27 @@
-let eightball = [
-    "Cant tell you.",
-    "Whats your problem ?, I am busy.",
-    "Go somewhere else.",
-    "Do you want me to abuse ?",
-    "Dont you have real people to talk to ?",
-    "I cant answer that.",
-    "Thanks for asking"
-];
+var verbs, nouns, adjectives, adverbs, preposition;
+nouns = ["bird", "clock", "boy", "plastic", "duck", "teacher", "old lady", "professor", "hamster", "dog"];
+verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"];
+adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
+adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+
+function randGen() {
+    return Math.floor(Math.random() * 5);
+}
+
+function sentence() {
+    var rand1 = Math.floor(Math.random() * 10);
+    var rand2 = Math.floor(Math.random() * 10);
+    var rand3 = Math.floor(Math.random() * 10);
+    var rand4 = Math.floor(Math.random() * 10);
+    var rand5 = Math.floor(Math.random() * 10);
+    var rand6 = Math.floor(Math.random() * 10);
+    var content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
+    return content;
+};
 
 module.exports = async (msg, args) => {
     if (!args.length) return;
-    const i = Math.floor(Math.random() * eightball.length);
-    const reply = eightball[i];
+    const reply = sentence();
     await msg.channel.send(`${msg.author} ${reply}`);
 }
